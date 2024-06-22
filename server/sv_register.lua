5fe4fbfd61f262b94363210cc6eb6a4d2f57b3ad
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2024-06-21 20:41:16 -0700

    forgot to rename job.name

--- server/sv_register.lua
+++ server/sv_register.lua
@@ -4,7 +4,7 @@ RegisterNetEvent("bd-henhousejob:server:billPlayer", function(playerId, amount)
     local biller = QBCore.Functions.GetPlayer(source)
     local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
     local amount = tonumber(amount)
-    if biller.PlayerData.job.name == 'hornysburger' then
+    if biller.PlayerData.job.name == 'henhouse' then
         if billed ~= nil then
             if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
                 if amount and amount > 0 then
